92a9d6a9460339085bf6c91fe42324c6c83080bf
Sourabh Jadhav <sourabh.jadhav@techenvo.com>
2024-11-19 22:57:04 +0530

    implemented customer entity,.,,,!

--- src/main/java/com/example/ordermanagement/model/Customer.java
+++ src/main/java/com/example/ordermanagement/model/Customer.java
@@ -1,15 +1,21 @@
 package com.example.ordermanagement.model;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name = "customer")
 public class Customer {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
+    private long id;
+
     private String name;
+
     private String email;
+
     private String address;
-    private String phone;
-    
-    // Getters and Setters
+
+    private String mobile;
 }
