1d8321d9fa29c041e9188b75ec7009c4e7064503
Sourabh Jadhav <sourabh.jadhav@techenvo.com>
2024-11-19 23:14:04 +0530

    implemented OrderRepository....!

--- src/main/java/com/example/ordermanagement/repository/OrderRepository.java
+++ src/main/java/com/example/ordermanagement/repository/OrderRepository.java
@@ -1,3 +1,7 @@
-public interface OrderRepository {
-    
+import com.psj.onlineGrocery.Entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
