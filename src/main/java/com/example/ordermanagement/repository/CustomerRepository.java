ca9c5caf87c6ab471bcb2f5d28643e4cef458ebb
Sourabh Jadhav <sourabh.jadhav@techenvo.com>
2024-11-19 23:14:27 +0530

    implemented CustomerRepository....!

--- src/main/java/com/example/ordermanagement/repository/CustomerRepository.java
+++ src/main/java/com/example/ordermanagement/repository/CustomerRepository.java
@@ -1,3 +1,8 @@
-public interface CustomerRepository {
-    
+import com.psj.onlineGrocery.Entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
+
