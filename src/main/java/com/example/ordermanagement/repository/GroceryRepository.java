3ff0c2f8db27ec3e168605924873f27db2138aff
Sourabh Jadhav <sourabh.jadhav@techenvo.com>
2024-11-19 23:14:12 +0530

    implemented GroceryRepository....!

--- src/main/java/com/example/ordermanagement/repository/GroceryRepository.java
+++ src/main/java/com/example/ordermanagement/repository/GroceryRepository.java
@@ -1,3 +1,7 @@
-public interface GroceryRepository {
-    
+import com.psj.onlineGrocery.Entities.Grocery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroceryRepository extends JpaRepository<Grocery, Long> {
 }
