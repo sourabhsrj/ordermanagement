2c2362aac5d56dc752e6aad8e9bbe7f0c77492b7
Sourabh Jadhav <sourabh.jadhav@techenvo.com>
2024-11-19 23:00:41 +0530

    implemented order entity.....!

--- src/main/java/com/example/ordermanagement/model/Order.java
+++ src/main/java/com/example/ordermanagement/model/Order.java
@@ -1,3 +1,26 @@
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "order")
 public class Order {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private Customer customer;
+
+    @ManyToMany
+    @JoinTable(
+            name = "order_groceryIterms",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "grocery_id")
+    )
+    private List<Grocery> groceries;
+
+    private Date date;
+
+    private double price;
 }
