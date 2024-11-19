247001131ac2292df94c1940bc9d2f6d28405da7
Sourabh Jadhav <sourabh.jadhav@techenvo.com>
2024-11-19 23:00:54 +0530

    implemented grocery entity.....!

--- src/main/java/com/example/ordermanagement/model/Grocery.java
+++ src/main/java/com/example/ordermanagement/model/Grocery.java
@@ -1,3 +1,22 @@
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "grocery")
 public class Grocery {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String category;
+
+    private int price;
+
+    private String quantity;
 }
