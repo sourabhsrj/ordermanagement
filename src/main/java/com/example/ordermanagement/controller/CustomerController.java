2baeef8015e17ace669842ff19dbf9d96bf28952
Sourabh Jadhav <sourabh.jadhav@techenvo.com>
2024-11-20 08:42:57 +0530

    implemented crud/rest api for CustomerController...!

--- src/main/java/com/example/ordermanagement/controller/CustomerController.java
+++ src/main/java/com/example/ordermanagement/controller/CustomerController.java
@@ -1,3 +1,68 @@
+
+import com.psj.onlineGrocery.Entities.Customer;
+import com.psj.onlineGrocery.Services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
-    
+
+    @Autowired
+    CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer){
+        try {
+            Customer createdCostomer = customerService.createCustomer(customer);
+            return new ResponseEntity<>(createdCostomer, HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers(){
+        List<Customer> customers = customerService.getAllCustomers();
+        if (!customers.isEmpty()) {
+            return new ResponseEntity<>(customers, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(customers, HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable Long id){
+        try {
+            Customer customer = customerService.getCustomer(id);
+            return new ResponseEntity<>(customer, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @RequestBody Customer customer){
+        try{
+            Customer updatedCustomer = customerService.updateCustomer(customer, id);
+            return new ResponseEntity<>(updatedCustomer, HttpStatus.OK);
+        }catch (RuntimeException e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable Long id){
+        try{
+            customerService.deleteUser(id);
+            return new ResponseEntity<>("Customer deleted successfully", HttpStatus.OK);
+        }catch (RuntimeException e){
+            return new ResponseEntity<>("Customer not found", HttpStatus.NOT_FOUND);
+        }
+    }
 }
+
