0e6123436f72d692faa0a53dd08b8996bc880b35
Sourabh Jadhav <sourabh.jadhav@techenvo.com>
2024-11-20 08:03:27 +0530

    added abstract menthods for GroceryService.....!

--- src/main/java/com/example/ordermanagement/services/GroceryService.java
+++ src/main/java/com/example/ordermanagement/services/GroceryService.java
@@ -1,3 +1,18 @@
+import com.psj.onlineGrocery.Entities.Grocery;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface GroceryService {
-    
+
+    List<Grocery> getAllGroceries();
+
+    Grocery getGrocery(Long id);
+
+    Grocery createGrocery(Grocery grocery);
+
+    Grocery updateGrocery(Grocery grocery, Long id);
+
+    void deleteGrocery(Long id);
 }
