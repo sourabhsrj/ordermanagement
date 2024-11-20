f535d4a0c42fc156228aa9be590870c24d7200b0
Sourabh Jadhav <sourabh.jadhav@techenvo.com>
2024-11-20 08:03:40 +0530

    added abstract menthods for CustomerService.....!

--- src/main/java/com/example/ordermanagement/services/CustomerService.java
+++ src/main/java/com/example/ordermanagement/services/CustomerService.java
@@ -1,3 +1,18 @@
+import com.psj.onlineGrocery.Entities.Customer;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface CustomerService {
-    
+
+    List<Customer> getAllCustomers();
+
+    Customer getCustomer(Long id);
+
+    Customer createCustomer(Customer customer);
+
+    Customer updateCustomer(Customer customer, Long id);
+
+    void deleteUser(Long id);
 }
