53641ca1b9c2ce49803651d7da80fb29cd187c8c
Sourabh Jadhav <sourabh.jadhav@techenvo.com>
2024-11-20 08:03:15 +0530

    added abstract menthods for OrderService.....!

--- src/main/java/com/example/ordermanagement/services/OrderService.java
+++ src/main/java/com/example/ordermanagement/services/OrderService.java
@@ -1,3 +1,18 @@
+import com.psj.onlineGrocery.Entities.Order;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface OrderService {
-    
+
+    List<Order> getAllOrders();
+
+    Order getOrder(Long id);
+
+    Order createOrder(Order order);
+
+    Order updateOrder(Order order, Long id);
+
+    void deleteOrder(Long id);
 }
